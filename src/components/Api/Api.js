25839d4ba7ebc61cde2132caecdced5165ac5a02
dankarger <karger.dan@gmail.com>
2021-12-23 23:55:57 +0200

    first commit

---
+++ src/components/Api/Api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export const axiosApi = axios.create({ baseURL: 'https://61c2f2dc9cfb8f0017a3e7c8.mockapi.io/shoes', });
+const API='https://61c2f2dc9cfb8f0017a3e7c8.mockapi.io/shoes/'
+
+export const getDataBase= async ()=>{
+    const DATA = await axiosApi.get('');
+    console.log('ds',DATA)
+    return DATA
+}
---
+++ src/components/ShoeApp/ShowApp.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {getDataBase} from "../Api/Api";
+import {BrowserRouter, Route, Link, Routes} from "react-router-dom";
+import HomePage from "../../pages/HomePage/HomePage";
+
+import './ShoeApp.css'
+
+
+
+class ShoeApp extends React.Component {
+        state = {data:[]}
+
+
+    componentDidMount() {
+            const DATA = getDataBase();
+            console.log('dd',DATA)
+            this.setState({data:DATA.data})
+    }
+
+
+    render() {
+        return(
+            <div>
+                <BrowserRouter >
+                    <Routes>
+                        <Route path='/' element={<HomePage/>} />
+
+                    </Routes>
+
+
+                </BrowserRouter>
+
+            </div>
+        )
+    }
+
+}
+export default ShoeApp
