08191b93c1a3e669118891f4b659712307ffe1b8
dankarger <karger.dan@gmail.com>
2021-12-24 22:37:18 +0200

    style buttons

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,17 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import './NavBat.css'
 
 
 
 export default function NavBar() {
 
     return(
-        <div>
+        <div id='NavBar'>
             <h1> <span><i className="fas fa-shoe-prints"></i></span> Shoe Store </h1>
             <div className="ui three item menu">
-                <Link to='/' className="item">Home</Link>
-                <Link  to='/create/new' className="item">Add Shoe</Link>
+                <Link to='/' className="item link">Home</Link>
+                <Link  to='/create/new' className="item link">Add Shoe</Link>
                 {/*<Link className="item">Upcoming Events</Link>*/}
             </div>
         </div>
